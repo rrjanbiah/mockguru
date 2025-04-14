dd2d514e3559dfecd19199351664d4764843acd7
R. Rajesh Jeba Anbiah <4907427+rrjanbiah@users.noreply.github.com>
2025-04-14 15:54:38 +0530

    feat: configure basePath and assetPrefix for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.DEPLOY_TARGET === "gh-pages";
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export", // Enable static export
+  basePath: isGithubPages ? "/mockguru" : "", // Set basePath for GitHub Pages
+  assetPrefix: isGithubPages ? "/mockguru/" : "", // Set assetPrefix for GitHub Pages
+  trailingSlash: true, // Good practice for static export
 };
 
 export default nextConfig;
