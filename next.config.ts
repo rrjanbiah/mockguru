64a62a9cc5503bf53cfe6b681e6ec4fe9c5e5fbc
R. Rajesh Jeba Anbiah <4907427+rrjanbiah@users.noreply.github.com>
2025-04-14 15:37:25 +0530

    feat: update GitHub Actions workflow for Next.js deployment and adjust package.json scripts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
+  output: "export", // Enable static export
 };
 
 export default nextConfig;
