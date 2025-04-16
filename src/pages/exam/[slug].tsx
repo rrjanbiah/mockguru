1dac66f75d70eab7050ae8fa18f4ff45dfde5f06
R. Rajesh Jeba Anbiah <4907427+rrjanbiah@users.noreply.github.com>
2025-04-16 23:55:31 +0530

    feat: replace old favicon path and update Open Graph image for improved SEO

--- src/pages/exam/[slug].tsx
+++ src/pages/exam/[slug].tsx
@@ -26,7 +26,7 @@ export default function ExamPage({
           <link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
-          <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
           <link rel="manifest" href="/site.webmanifest" />
           <meta property="og:image" content={`${process.env.SITE_URL}/img/og-image.png`} />
           <meta property="og:title" content={`${frontmatter.title} | MockGuru`} /> {/* Updated for SEO */}
